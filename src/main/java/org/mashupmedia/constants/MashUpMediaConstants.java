a95a8c6adabfd4b2b13c99e99437796bbb6534c2
Andy <andy.lester@smallworldsolutions.eu>
2013-02-17 21:20:06 +0100

    working on playlist rules

--- src/main/java/org/mashupmedia/constants/MashUpMediaConstants.java
+++ src/main/java/org/mashupmedia/constants/MashUpMediaConstants.java
@@ -18,6 +18,7 @@ public interface MashUpMediaConstants {
 	public static final String UNKNOWN_NAME = "unknown";
 	
 	public static final String MODEL_KEY_THEME_PATH = "themePath";
+	public static final String MODEL_KEY_IS_PLAYLIST_OWNER = "isPlaylistOwner";
 	public static final String PROXY_CACHE_SECONDS = "proxy.cache.seconds";
 	public static final String VOTE_USER_WAIT_SECONDS = "vote.user.wait.seconds";
 	
--- src/main/java/org/mashupmedia/controller/ajax/BaseAjaxController.java
+++ src/main/java/org/mashupmedia/controller/ajax/BaseAjaxController.java
@@ -1,13 +1,43 @@
 package org.mashupmedia.controller.ajax;
 
 import org.mashupmedia.constants.MashUpMediaConstants;
+import org.mashupmedia.model.User;
+import org.mashupmedia.model.playlist.Playlist;
+import org.mashupmedia.model.playlist.Playlist.PlaylistType;
+import org.mashupmedia.service.PlaylistManager;
+import org.mashupmedia.util.SecurityHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 public abstract class BaseAjaxController {
+	
+	@Autowired
+	private PlaylistManager playlistManager;
 
 	@ModelAttribute(MashUpMediaConstants.MODEL_KEY_THEME_PATH)
 	public String getThemePath() {
 		return "/themes/default";
 	}
+	
+	@ModelAttribute(MashUpMediaConstants.MODEL_KEY_IS_PLAYLIST_OWNER)
+	public boolean isPlaylistOwner() {
+		Playlist playlist = playlistManager.getLastAccessedPlaylistForCurrentUser(PlaylistType.ALL);
+		User createdBy = playlist.getCreatedBy();
+		User user = SecurityHelper.getLoggedInUser();
+		
+		// If the createdBy is null presume that the user has just created this playlist
+		if (createdBy == null) {
+			return true;
+		}
+		
+		if (createdBy.equals(user)) {
+			return true;
+		}
+		
+		return false;
+	
+	}
+	
+	
 
 }
