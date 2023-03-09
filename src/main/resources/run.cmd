4414aa4fbe3787a61ce6833b7de92815a1d825a6
Andy Lester <andrew.lester@minfin.fed.be>
2023-03-09 10:07:17 +0100

    colour

--- src/main/resources/run.cmd
+++ src/main/resources/run.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF 
 
-java -DmashupMediaHome="C:\stuff\data\mm" -Dserver.port=8080 -jar mashupmedia-server\target\mashupmedia-server-4.0.0-RELEASE.jar
+java -DmashupMediaHome="C:\stuff\data\mm" -Dserver.port=8080 -jar ..\..\..\mashupmedia-server\target\mashupmedia-server-4.0.0-RELEASE.jar
